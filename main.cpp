2309c9d531a1d00126b04295c90cab4998009eb5
itzandroidtab <koen@koon.io>
2021-09-26 03:21:13 +0200

    cleanup of main and moved m05 to a class

--- main.cpp
+++ main.cpp
@@ -1,133 +1,114 @@
 #include <windows.h>
 #include <iostream>
-#include <cstdint>
-#include <string>
 #include <algorithm>
 
-#include <mpc05ls.hpp>
-
-int file_exists(std::string filename) {
-    int file_id, file_num;
-
-    // get the amount of files on the machine
-    M05_get_file_num(&file_id, &file_num);
-
-    // check if there are files on the machine
-    if (file_num < 0) {
-        return -1;
-    }
-
-    long unsigned int file_len;
-    uint8_t local_file[64] = {};
-
-    // check if a file with the name already exists
-    for (size_t i = 0; i < file_id; i++) {
-        // open the file
-        M05_open_file(i + 1, &file_len, local_file);
-
-        // convert the local filename to a string
-        auto l = std::string(reinterpret_cast<char*>(local_file));
-
-        // check if the local filename matches the name we are looking for
-        if (l == filename) {
-            // found the id we are looking for
-            return i + 1;
-        }
-    }
-
-    return -1;
-}
+#include <mpc65xx.hpp>
 
 int main(int argc, char ** argv) {
-    // set the communication
-    M05_set_comm(0);
-
-    // try to open the link to the laser machine
-    M05_open_link(1, 115200);
-
-    // init the laser board/library
-    if (M05_init_board()) {
-        std::cout << "Something went wrong initilizing the board" << std::endl;
-
-        // close the connection
-        M05_close_link(1);
+    // check if we have a file as argument
+    if (argc < 2) {
+        std::cerr << "Not enough arguments. Usage:" << std::endl
+                  << "text_to_mol file" << std::endl;
 
         return -1;
     }
 
-    // get the filename without any path or extention
-    std::string filepath = argv[1];
+    // create a mpc object
+    mpc::mpc65xx mpc65xx(1, 115200);
 
-    // compile if we are getting called with a txt file
-    if (filepath.find(".TXT") != std::string::npos) {
-        // create a copy of the filepath
-        auto *c = strdup(filepath.c_str());
+    // try to open the connection
+    if (!mpc65xx.open()) {
+        std::cerr << "Something went wrong initilizing the board" << std::endl;
 
-        // compile the file. This writes to the same folder with a .MOL extention
-        (void) M05_compile_work_file(argv[1]);
-
-        // free the char array memory
-        free(c);
+        // early return to prevent execution of code below
+        return -1;
     }
 
+    // get a string version of the argument
+    std::string file_argv = std::string(argv[1]);
+
     // search for the extention dot
-    auto found = filepath.find_last_of(".");
+    auto found = file_argv.find_last_of(".");
+
+    // get the extention of the file
+    std::string extention;
 
     // check if we can strip the extention
     if (found != std::string::npos) {
         // strip the extention
-        filepath = filepath.substr(0, found);
+        extention = file_argv.substr(0, found);
     }
 
-    // add the .mol extention
-    filepath += ".MOL";
+    // compile if we are getting called with a txt file
+    if (extention.find(".TXT") != std::string::npos || 
+        extention.find(".txt") != std::string::npos) {
+        std::cout << "Compiling file." << std::endl;
+
+        // create a copy of the argument
+        char *file_path_buffer = strdup(file_argv.c_str());
+
+        // compile the file. This writes to the same folder with a .MOL extention
+        (void) M05_compile_work_file(file_path_buffer);
+
+        // free the char array memory
+        free(file_path_buffer);
+
+        // change the extention to a .mol extention
+        extention += ".MOL";        
+    }
+
+    // split the filepath, filename and extention.
+    std::string path;
+    std::string filename;
 
     // check if we have a filename specified
-    auto f = filepath.find_last_of("/\\");
-
-    std::string filename = "";
+    auto path_result = file_argv.find_last_of("/\\");
 
     // check what we need to do with the filename and path
-    if (f != std::string::npos) {
-        // get the filename from the filepath
-        filename = filepath.substr(f + 1);
+    if (path_result != std::string::npos) {
+        // get the path
+        path = file_argv.substr(0, path_result);
 
-        // remove the filename
-        filepath = filepath.substr(0, f);
+        // get the filename
+        filename = file_argv.substr(path_result + 1);
     }
     else {
-        // set the filename as the filepath if we dont have any \\ or /
-        filename = filepath;
-
         // clear the filepath
-        filepath = "";
+        path = "";
+
+        // set the filename as the filepath if we dont have any \ or /
+        filename = file_argv;        
     }
 
     // change the filename to upper
-    for (auto & c: filename) {
+    for (auto &c: filename) {
         c = toupper(c);
     }    
 
-    // check if the file already exists
-    auto r = file_exists(filename);
+    // get al the files
+    auto files = mpc65xx.get_files();
+
+    // check if the file already exists on the machine
+    auto find_result = std::find_if(files.begin(), files.end(), [&filename](mpc::file_s file) {
+        // return if the file name is the same
+        return filename == file.file;
+    });
 
     // delete the file if it exists
-    if (r >= 0) {
+    if (find_result != files.end()) {
         // delete the file
-        M05_del_file(r);
+        mpc65xx.delete_file((*find_result).id);
     }
 
-    // create a copy of the filepath + filename (add a \ in between to fix the M05 bug with strange names)
-    auto *c = strdup((filepath + "\\" + filename).c_str());
+    std::cout << "Uploading file." << std::endl;
 
-    // upload the file 
-    M05_download_work_file(c);
+    // upload the filepath + filename (add a \ in between to fix the M05 bug with strange names)
+    mpc65xx.upload_file(path + "\\" + filename);
 
-    // free the memory
-    free(c);
+    std::cout << "Done uploading file." << std::endl;
 
     // close the link
-    M05_close_link(1);
+    mpc65xx.close();
 
     return 0;
 }
---
+++ mpc65xx.hpp
@@ -0,0 +1,152 @@
+#ifndef MPC65XX_HPP
+#define MPC65XX_HPP
+
+#include <vector>
+#include <cstdint>
+#include <string>
+
+#include <mpc05ls.hpp>
+
+namespace mpc {
+    struct file_s {
+        // name of the file
+        std::string file;
+
+        // length of the file
+        uint64_t length;
+
+        // id of the file
+        uint32_t id;
+    };
+
+    class mpc65xx {
+        protected:
+            // channel to the mpc65xx motion card
+            const int channel;
+
+            // rate of the mpc65xx motion card 
+            const int rate;
+
+        public:
+            mpc65xx(const int channel = 1, const int rate = 115200): 
+                channel(channel), rate(rate)
+            {}
+
+            /**
+             * @brief Open the communication to the mpc65xx motion card
+             * 
+             * @return true 
+             * @return false 
+             */
+            bool open() {
+                // set the communication
+                M05_set_comm(nullptr);
+
+                // try to open the link to the laser machine
+                M05_open_link(channel, rate);
+
+                // init and check if result is true (init went wrong)
+                if (M05_init_board()) {
+                    // close the connection
+                    M05_close_link(channel);
+
+                    // init went wrong return
+                    return false;
+                }
+
+                return true;
+            }
+
+            /**
+             * @brief Close the communication to the mpc65xx motion card
+             * 
+             */
+            void close() {
+                // close the link
+                M05_close_link(channel);            
+            }
+
+            /**
+             * @brief Get the amount of files on the mpc65xx motion card
+             * 
+             * @return uint32_t 
+             */
+            uint32_t get_file_amount() {
+                // create the variables for the m05 func
+                int file_id, amount;
+
+                // get the amount of files on the machine
+                M05_get_file_num(&file_id, &amount);
+
+                // return a minimum of 0
+                return std::max(amount, 0);
+            }
+
+            /**
+             * @brief Get all the files on the mpc65xx motion card
+             * 
+             * @return std::vector<std::string> 
+             */
+            std::vector<file_s> get_files() {
+                // get the amount of files
+                uint32_t amount = get_file_amount();
+
+                // check if we have files on the machine
+                if (!amount) {
+                    // we have no files return
+                    return {};
+                }
+
+                // return value
+                std::vector<file_s> ret;
+
+                // iterate over all the files
+                for (uint32_t index = 1; index <= amount; index++) {
+                    // lenght for the file
+                    long unsigned int len = 0;
+
+                    // pointer to a char array for the file name
+                    uint8_t file_name[128] = {};
+
+                    // open the file
+                    M05_open_file(index, &len, file_name);
+
+                    // create a file struct
+                    file_s file = {
+                        std::string(reinterpret_cast<const char*>(file_name)),
+                        len, index
+                    };
+
+                    // add the file to the vector
+                    ret.push_back(file);
+                }
+
+                // return the vector with the files
+                return ret;
+            }
+
+            void delete_file(int id) {
+                // check if we have a valid id
+                if (id > get_file_amount()) {
+                    // id not valid return
+                    return;
+                }
+
+                // delete the file
+                M05_del_file(id);                
+            }
+
+            void upload_file(std::string file) {
+                // create a copy of the file for the m05 func
+                auto *c = strdup(file.c_str());
+
+                // upload the file 
+                M05_download_work_file(c);
+
+                // free the memory
+                free(c);
+            }
+    };
+}
+
+#endif
